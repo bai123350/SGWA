8b4e12b93979e5b00551795fbba2b3e2c977086f
bai123350 <1375652881@qq.com>
2025-07-08 16:28:33 +0800

    init

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' An example Seurat v5 object
+#'
+#' A dataset containing simulated single-cell RNA-seq data with 1000 genes
+#' and 200 cells. The object includes metadata for a simulated treatment.
+#'
+#' @format A Seurat object with the following assays:
+#' \describe{
+#'   \item{RNA}{RNA assay with 1000 features and 200 cells}
+#' }
+#' @source Generated from `tmp/seurat_data.R`
+"seurat"
